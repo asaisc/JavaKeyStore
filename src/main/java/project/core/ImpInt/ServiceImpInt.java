68f956967f214023f8873ef92e0836e091801686
asaisc <asael_09@hotmail.com>
2018-07-28 18:05:49 -0500

    Cambios para obtener Clave Publica de un Java KeyStore

--- src/main/java/project/core/ImpInt/ServiceImpInt.java
+++ src/main/java/project/core/ImpInt/ServiceImpInt.java
@@ -1,14 +1,25 @@
 package project.core.ImpInt;
 
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import project.core.Int.ServiceInt;
 
 public class ServiceImpInt implements ServiceInt {
 
 	public RSAPrivateKey getPrivateKey(String JKSFileName, String JKSAlias, String JKSPassword) {
-		// TODO Auto-generated method stub
-		return null;
+		RSAPrivateKey privateKey = null;
+		return privateKey;
+	}
+
+	public RSAPublicKey getPublicKey(String JKSFileName) {
+		RSAPublicKey publicKey = null;
+		return publicKey;
+	}
+
+	public String firmar(String string) {
+		String firma = "";
+		return firma;
 	}
 
 }
--- src/main/java/project/core/Int/ServiceInt.java
+++ src/main/java/project/core/Int/ServiceInt.java
@@ -1,7 +1,10 @@
 package project.core.Int;
 
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 public interface ServiceInt {
 	public RSAPrivateKey getPrivateKey(String JKSFileName, String JKSAlias, String JKSPassword);
+	public RSAPublicKey getPublicKey(String JKSFileName);
+	public String firmar(String string);
 }
